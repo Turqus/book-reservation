52a75c42fdbcb124228d94f4ee96d073b36dd3ca
Turqus <bartek@x-world.pl>
2018-01-20 23:04:02 +0100

    added login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wypożyczalnia Książek' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Logowanie', error_msg: [] });
 });
 
 module.exports = router;
